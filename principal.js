c9bb09b91254973fd1ad8ec1655085534f0c14dd
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2022-12-03 16:31:50 -0300

    Opening page banner

--- principal.js
+++ principal.js
@@ -3,8 +3,8 @@ var banners = ["Os melhores do Brasil!", "Qualidade e preço baixo"]
 var banner = 0
 
 function trocaBanner() {
-    bannerAtual = (bannerAtual + 1) % 2
+    bannerAtual = (banner + 1) % 2
     document.querySelector("h2#message").textContent = banners[bannerAtual]
 }
 
-setInterval(trocaBanner, 2000)
+setInterval(trocaBanner, 1000)
